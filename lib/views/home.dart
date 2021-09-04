764b28477b773af11ffe393c352bbdc89f444275
Plane <plane2001@outlook.com>
2021-09-04 11:07:53 +0530

    Initial commit

---
+++ lib/views/home.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get_news/helper/data.dart';
+import 'package:get_news/models/category_model.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<CategoryModel> categories = <CategoryModel>[];
+
+  @override
+  void initState() {
+    // Init state will be called when the screen opens
+    // TODO: implement initState
+    super.initState();
+    categories = getCategories();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Get",
+              style: TextStyle(fontSize: 30.0),
+            ),
+            Text(
+              "News",
+              style: TextStyle(color: Colors.blue, fontSize: 30.0),
+            ),
+          ],
+        ),
+        elevation: 0.0,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: ListView.builder(
+                itemCount: categories.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return CategoryTile(
+                    imageURL: categories[index].imageURL,
+                    categoryName: categories[index].categoryName,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryTile extends StatelessWidget {
+  final imageURL, categoryName;
+
+  CategoryTile({this.imageURL, this.categoryName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: <Widget>[
+          Image.network(
+            imageURL,
+            width: 120.0,
+            height: 60.0,
+          ),
+        ],
+      ),
+    );
+  }
+}
